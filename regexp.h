3bd38403f88f0ad4e1b0aa6100c1c2b048f0604e
Shigeru Hagiwara <hgwrsgr@gmail.com>
2019-12-30 21:58:39 +0900

    import from https://mirrors.slackware.com/slackware/slackware-3.3/source/a/bin/elvis-1.8pl3.tar.gz

---
+++ regexp.h
@@ -0,0 +1,21 @@
+/*
+ * Definitions etc. for regexp(3) routines.
+ *
+ * Caveat:  this is V8 regexp(3) [actually, a reimplementation thereof],
+ * not the System V one.
+ */
+#define NSUBEXP  10
+
+typedef struct regexp {
+	char	*startp[NSUBEXP];
+	char	*endp[NSUBEXP];
+	int	minlen;		/* length of shortest possible match */
+	char	first;		/* first character, if known; else \0 */
+	char	bol;		/* boolean: must start at beginning of line? */
+	char	program[1];	/* Unwarranted chumminess with compiler. */
+} regexp;
+
+extern regexp *regcomp P_((char *));
+extern int regexec P_((regexp *, char *, int));
+extern void regsub P_((regexp *, char *, char *));
+extern void regerror P_((char *));
